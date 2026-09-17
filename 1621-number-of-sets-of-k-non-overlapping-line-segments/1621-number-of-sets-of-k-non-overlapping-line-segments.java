04e68af7ad726c2970cbffc515c203af90e22b34
Chahat Jain <chahatjain031@gmail.com>
2026-09-17 10:48:25 +0530

    Time: 55 ms (52.63%), Space: 153.3 MB (38.82%) - LeetHub

--- 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
+++ 1621-number-of-sets-of-k-non-overlapping-line-segments/1621-number-of-sets-of-k-non-overlapping-line-segments.java
@@ -1,34 +1,45 @@
-import java.util.Arrays;
-
 class Solution {
     int MOD = 1_000_000_007;
+
+    // dp[k][i] =
+    // point i se exactly k segments banane ke ways
     int[][] dp = new int[1001][1001];
 
     public int numberOfSets(int n, int K) {
 
+        // Exactly 0 segments ke ways
         for (int i = 0; i <= n; i++) {
             dp[0][i] = (i < n) ? 1 : 0;
         }
 
+        // Pehle 1 segment, phir 2, ..., K segments
         for (int k = 1; k <= K; k++) {
 
+            // Previous DP row ke suffix sums store karenge
             int[] prevRowSum = new int[n + 1];
 
+            // Right se left suffix sum calculate karna
             for (int x = n - 1; x >= 0; x--) {
                 prevRowSum[x] =
                     (prevRowSum[x + 1] + dp[k - 1][x]) % MOD;
             }
 
+            // Current DP row ko right se left fill karna
             for (int i = n - 1; i >= 0; i--) {
 
+                // Point i ko skip karo
                 int skip = dp[k][i + 1];
 
+                // Point i par segment start karo
+                // Required sum already stored hai
                 int take = prevRowSum[i + 1];
 
+                // Total ways = skip + take
                 dp[k][i] = (take + skip) % MOD;
             }
         }
 
+        // Point 0 se exactly K segments
         return dp[K][0];
     }
 }
