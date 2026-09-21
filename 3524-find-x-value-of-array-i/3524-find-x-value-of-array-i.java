9a027fd1c7687bdc03fef37cb5f3a4c88962eff7
Chahat Jain <chahatjain031@gmail.com>
2026-09-21 10:51:30 +0530

    Time: 15 ms (32.69%), Space: 93.5 MB (48.08%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.java
@@ -1,14 +1,14 @@
 class Solution {
     public long[] resultArray(int[] nums, int k) {
-        long[] dp=new long[k];
+        long[] dp=new long[k];//no of subarrays ending at prev element having this prod remaiinder
         long[] ans= new long[k];
         for(int num:nums){
-            long[] newDp=new long[k];
+            long[] newDp=new long[k];//subarrays ending at current elemnt
             int numMod=num%k;
             newDp[numMod]++;
             for(int i=0;i<k;i++){
                 int newRem=(int)((long) i*numMod%k);
-                newDp[newRem]+=dp[i];
+                newDp[newRem]+=dp[i];//oldsubaray + current no=new subarray
             }
             for(int i=0;i<k;i++){
                 ans[i]+=newDp[i];
